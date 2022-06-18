839b7672184d15c2129f974520d71bb47d8edf9b
lamgr <lamgradon@Hotmail.com>
2022-06-18 16:28:36 +0800

    [misc] 翻译文本

--- src/CommandSystem/Enum/Commands.java
+++ src/CommandSystem/Enum/Commands.java
@@ -4,48 +4,48 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 public enum Commands {
-    LOGIN(2, "use to login", "login <login_beacon> <password>"), // visitor
-    REGISTER(3, "register a new account", "register <login_beacon> <username> <password>"), // visitor
-    CAR(1, "use to operate car info",
+    LOGIN(2, "用于登录", "login <登录名> <密码>"), // visitor
+    REGISTER(3, "注册新账号", "register <登录名> <用户名> <密码>"), // visitor
+    CAR(1, "用于操作车辆信息",
             "CAR_CREATE, CAR_QUERY, CAR_REMOVE, CAR_MODIFY, CAR_LIST, CAR_LISTBY, CAR_LISTALL, CAR_MODIFY_ADMIN"
     ),
-    ADMIN(1, "use to operate admin command",
+    ADMIN(1, "用于管理员权限",
             "USER, CAR"
     ),
-    PROFILE(1, "use this command to modify your profile",
+    PROFILE(1, "用于个人资料修改",
             "PROFILE_MODIFY, PROFILE_SHOW"),
-    USER(1, "for admin only, use to operate user info",
+    USER(1, "管理员命令列表",
             "USER_CREATE, USER_REVOKE, USER_MODIFY, USER_LIST"), // empty command set, for verify
-    CAR_CREATE(3, "use this command to create a car", "car create <id> <model> <comment>"),
-    CAR_QUERY(1, "query a car using its ID and return its info", "car query <id>"),
-    CAR_REMOVE(1, "remove a car from your account, or remove from any as a admin",
+    CAR_CREATE(3, "用于创建车辆信息", "car create <id> <车辆型号> <备注>"),
+    CAR_QUERY(1, "通过车牌ID查询信息", "car query <id>"),
+    CAR_REMOVE(1, "删除你所属车辆信息，或者以管理员身份删除任意车辆",
             "car remove <id>;admin car remove <id>"),
-    CAR_MODIFY(3, "mod a car info from your account, or from any as a admin",
-            "car mod <id> <model> <comment>;car mod <id> <owner> <model> <comment>"),
-    CAR_LIST(0, "list all car from your account",
+    CAR_MODIFY(3, "修改你所属车辆信息，或者以管理员身份修改任意车辆",
+            "car mod <id> <车辆型号> <备注>;car mod <id> <拥有者> <车辆型号> <备注>"),
+    CAR_LIST(0, "列出你名下的所有车辆信息",
             "car list"), // common
-    CAR_LISTBY(1, "admin only, list all car from specific account",
-            "admin car listby <owner>"),
-    CAR_LISTALL(0, "admin only, list all car from database, same as car list as admin",
+    CAR_LISTBY(1, "管理员指令，列出任意用户的车辆信息",
+            "admin car listby <拥有者>"),
+    CAR_LISTALL(0, "管理员指令，列出所有车辆信息",
             "admin car listall"), // admin
-    CAR_MODIFY_ADMIN(4, "admin only, mod a car information from any account",
-            "admin car mod <id> <owner> <model> <comment>"), // car mod admin variant
-    PROFILE_MODIFY(2, "mod your profile, including username and password",
-            "profile mod <username> <password>"),
-    PROFILE_SHOW(0, "display your profile info",
+    CAR_MODIFY_ADMIN(4, "管理员指令，修改任意账户下的任意车辆",
+            "admin car mod <id> <拥有者> <车辆型号> <备注>"), // car mod admin variant
+    PROFILE_MODIFY(2, "用于修改你的个人资料",
+            "profile mod <用户名> <密码>"),
+    PROFILE_SHOW(0, "显示你的个人资料",
             "profile show"), // common
-    USER_CREATE(4, "admin only, create a new user",
-            "admin user create <auth_level> <login_beacon> <username> <password>"),
-    USER_REVOKE(1, "admin only, delete(revoke) a user",
-            "admin user revoke <login_beacon>"),
-    USER_MODIFY(4, "admin only, mod a user's info",
-            "admin user mod <auth_level> <login_beacon> <username> <password>"), // admin
-    USER_LIST(0, "admin only, list all user", "admin user list"),
-    HELP(0, "display command's help info, display all with no param or display specific one with param",
-            "HELP [command]"),
-    LOGOUT(0, "use this to logout your current account", "logout"), // common, admin
-    EXIT(0, "shutdown and save all data", "EXIT"), // all
-    __INVALID_COMMAND__(0, "no such a command", "use HELP [command] for more info");
+    USER_CREATE(4, "管理员指令，创建用户",
+            "admin user create <权限等级> <登录名> <用户名> <密码>"),
+    USER_REVOKE(1, "管理员指令，删除用户",
+            "admin user revoke <登录名>"),
+    USER_MODIFY(4, "管理员指令，修改用户信息",
+            "admin user mod <权限等级> <登录名> <用户名> <密码>"), // admin
+    USER_LIST(0, "管理员指令，列出所有用户", "admin user list"),
+    HELP(0, "显示帮助，不带参数显示所有用法或者指定一个指令查看详细用法",
+            "help [指令]"),
+    LOGOUT(0, "退出当前帐号", "logout"), // common, admin
+    EXIT(0, "关闭系统并且保存所有数据修改", "exit"), // all
+    __INVALID_COMMAND__(0, "没有该指令", "使用 help [指令] 来查看更多详细信息");
 
     private final int paramsCount;
     private final String description;
@@ -102,23 +102,23 @@ public enum Commands {
         return usage;
     }
 
-    // login <login_beacon> <password>
-    // register <login_beacon> <username> <password>
-    // car create <id> <model> <comment>
+    // login <登录名> <密码>
+    // register <登录名> <用户名> <密码>
+    // car create <id> <车辆型号> <备注>
     // car query <id>
     // car remove <id>
     // car list
-    // car mod <id> <model> <comment>
+    // car mod <id> <车辆型号> <备注>
     // profile show
-    // profile mod <username> <password>
+    // profile mod <用户名> <密码>
 
-    // admin user create <auth_level> <login_beacon> <username> <password>
-    // admin user revoke <login_beacon>
-    // admin user mod <auth_level> <login_beacon> <username> <password>
+    // admin user create <权限等级> <登录名> <用户名> <密码>
+    // admin user revoke <登录名>
+    // admin user mod <权限等级> <登录名> <用户名> <密码>
     // admin user list
     // admin car remove <id>
-    // admin car listby <owner>
+    // admin car listby <拥有者>
     // admin car listall
-    // admin car mod <id> <owner> <model> <comment>
+    // admin car mod <id> <拥有者> <车辆型号> <备注>
 
 }
--- src/UI/HintMaker.java
+++ src/UI/HintMaker.java
@@ -14,16 +14,16 @@ import static UI.ConsoleLogger.*;
 public class HintMaker {
     public static void WELCOME_MESSAGE(){
         RETURN_CYAN_MESSAGE_BLOCK(
-                "Car Information Manage System",
-                "Version 0.9a",
-                "ANSI escape sequences supporting is required for best experience",
-                "Developed by Eticeweng, CJL and LJH, All rights reserved");
-        INFO("Please type help for usage help.");
+                "车辆信息管理系统",
+                "当前版本 0.9b_zh_cn",
+                "请使用支持ANSI 控制序列的终端来运行本应用以获得最佳的使用体验",
+                "由 Eticeweng, CJL 和 LJH 开发, 保留所有权利");
+        INFO("请输入 help 来查看使用帮助");
     }
 
     public static void FAILED_VERIFY_PRIVILEGE(){
-        ERROR("Permission denied.");
-        ERROR("The user group you belong to doesn't have the permission to execute this command.");
+        ERROR("权限不足");
+        ERROR("你所属的用户组没有权限使用该命令");
     }
 
     public static void INVALID_COMMAND_PATTERN(CommandPipeline commandPipeline){
@@ -31,12 +31,12 @@ public class HintMaker {
             FAILED_MATCH_PARAMS_PATTERN(commandPipeline);
             return;
         }
-        ERROR("Invalid sub command.");
-        ERROR(commandPipeline.getCommand() + " has no sub command: " + commandPipeline.getNextParam());
+        ERROR("子命令无效");
+        ERROR(commandPipeline.getCommand() + " 没有该子命令: " + commandPipeline.getNextParam());
     }
 
     public static void FAILED_MATCH_PARAMS_PATTERN(CommandPipeline command){
-        ERROR(String.format("%s expects %d params but received %d"
+        ERROR(String.format("%s 需要 %d 个参数但是却接受了 %d 个"
                 , command.getCommand()
                 , command.getCommand().getParamsCount()
                 , command.getParamsCount()));
@@ -44,40 +44,40 @@ public class HintMaker {
     }
 
     public static void INVALID_COMMAND(String cmd){
-        ERROR("No such a command: " + cmd);
+        ERROR("没有该命令: " + cmd);
     }
 
     public static void NOT_DUPLICATE_AUTH(){
-        WARN("You already logon.");
-        WARN("Please use \"logout\" to logout first.");
+        WARN("你已经登陆了");
+        WARN("请先使用 logout 来退出当前帐号");
     }
 
     public static void LOGON(AuthSession authSession){
-        INFO("Welcome, " + authSession.getUsername() + ".");
-        INFO("Your permission level is " + authSession.getAuthLevel().getAuthName() + ".");
+        INFO("欢迎回来, " + authSession.getUsername() + ".");
+        INFO("你的权限等级为 " + authSession.getAuthLevel().getAuthName() + ".");
     }
 
     public static void FAILED_LOGON(){
-        ERROR("Failed to login.");
-        ERROR("Please check your login name or password.");
+        ERROR("登陆失败");
+        ERROR("请检查你是否输入了正确的密码或者该账号已经注册");
     }
 
     public static void REGISTERED(AuthSession authSession){
-        INFO("Your account have successfully registered, and logon as you created.");
+        INFO("你已经成功注册，且已自动登录");
         SHOW_USER(authSession);
     }
 
     public static void FAILED_REGISTER(){
-        ERROR("Failed to register.");
-        ERROR("Please check your registration info is duplicated or not.");
+        ERROR("注册失败");
+        ERROR("你的ID已被注册");
     }
 
     public static void LOGOUT_MESSAGE(){
-        INFO("You have successfully logout, bye.");
+        INFO("账号已经退出，再见");
     }
 
     public static void EXIT_MESSAGE(){
-        INFO("System exited, see you.");
+        INFO("系统成功关闭，改日再会");
     }
 
     public static void RETURN_MESSAGE_BLOCK(String... messages){
@@ -89,100 +89,100 @@ public class HintMaker {
     }
 
     public static void SUCCEEDED_CREATE_CAR(Car car){
-        INFO("New car information has successfully created:");
+        INFO("你所指定的车辆信息已被成功注册:");
         SHOW_CAR(car);
     }
 
     public static void SHOW_CAR(Car car){
         RETURN_WHITE_VERTICAL_ALIGNED_BLOCK(
-                "Car ID: " + car.getID(),
-                "Owner: " + car.getOwner().toString(),
-                "Car Model: " + car.getModel(),
-                "Comment: " + car.getComment()
+                "车牌: " + car.getID(),
+                "拥有者: " + car.getOwner().toString(),
+                "型号: " + car.getModel(),
+                "备注: " + car.getComment()
         );
     }
 
     public static void FAILED_CREATE_CAR(){
-        ERROR("Failed to create car information.");
-        ERROR("The database might be full or the ID already registered.");
+        ERROR("车辆信息注册失败");
+        ERROR("请检查该ID是否已经被注册");
     }
 
     public static void NOT_FOUND_CAR(){
-        WARN("We cant find the car information you requested.");
-        WARN("Please check the car ID you enter, and try again.");
+        WARN("无法找你所指定的车辆信息");
+        WARN("请检查你输入的车牌然后再试一次");
     }
 
     public static void SUCCEEDED_REMOVE_CAR(Car car){
-        INFO("You have successfully remove the car you requested for.");
+        INFO("成功删除你指定的车辆信息");
         SHOW_CAR(car);
     }
 
     public static void NOT_REMOVE_CAR(){
-        WARN("We cant remove the car information you requested.");
-        WARN("Please make sure the car you requested belongs to you OR check the ID you enter.");
+        WARN("无法删除你所指定的车辆信息");
+        WARN("请确保你输入了正确的ID或者你输入的ID是属于你的");
     }
 
     public static void SUCCEEDED_MODIFY_CAR(Car car){
-        INFO("You have successfully modify this car information.");
+        INFO("车辆信息修改成功");
         SHOW_CAR(car);
     }
 
     public static void NOT_MODIFY_CAR(){
-        WARN("We can't help you to modify this car information.");
-        WARN("Please make sure the car you requested belongs to you OR check the ID you enter.");
+        WARN("无法修改你指定的车辆信息");
+        WARN("请确保你输入了正确的ID或者你输入的ID是属于你的");
     }
 
     public static void EMPTY_CAR_LIST(){
-        WARN("We found no car that belongs to you.");
+        WARN("你名下没有任何车辆信息");
     }
 
     public static void FAILED_MODIFY_USER(){
-        ERROR("Failed to modify user profile.");
-        ERROR("Something BAD might happened inside the database OR program logic.");
+        ERROR("无法修改指定用户的信息");
+        ERROR("BAD");
     }
 
     public static void SHOW_USER(AuthSession authSession){
         RETURN_WHITE_VERTICAL_ALIGNED_BLOCK(
-                "Permission level: " + authSession.getAuthLevel(),
+                "权限等级: " + authSession.getAuthLevel(),
                 "UUID: " + authSession.getUuid(),
-                "Login name: " + authSession.getLoginBeacon(),
-                "Username: " + authSession.getUsername(),
-                "Password(UUID encoded): " + UUID.nameUUIDFromBytes(authSession.getPassword().getBytes())
+                "登录名: " + authSession.getLoginBeacon(),
+                "用户名: " + authSession.getUsername(),
+                "密码(UUID编译后): " + UUID.nameUUIDFromBytes(authSession.getPassword().getBytes())
         );
     }
 
     public static void SUCCEEDED_MODIFY_USER(AuthSession authSession){
-        INFO("You have successfully modify your account information.");
+        INFO("成功修改用户信息");
         SHOW_USER(authSession);
     }
 
     public static void FAILED_ADMIN_CREATE_USER(){
-        ERROR("Failed to create user as admin.");
-        ERROR("Please check the capacity of the database OR is there a duplicated ID.");
+        ERROR("无法创建用户");
+        ERROR("请确保你输入了没有冲突的ID");
     }
 
     public static void SUCCEEDED_ADMIN_CREATE_USER(AuthSession authSession){
-        INFO("Create user as admin successfully.");
+        INFO("成功创建用户");
         SHOW_USER(authSession);
     }
 
     public static void NOT_ADMIN_REVOKE_USER(){
-        WARN("Unable to revoke this user.");
-        WARN("Please check the user ID you requested is exists or not.");
+        WARN("用户信息移除失败");
+        WARN("请确保你输入了正确的ID");
     }
 
     public static void SUCCEEDED_ADMIN_REVOKE_USER(AuthSession authSession) {
-        INFO("Successfully revoked this user.");
+        INFO("成功移除用户");
         SHOW_USER(authSession);
     }
 
     public static void NOT_ADMIN_MODIFY_USER(){
-        WARN("Unable to modify this user.");
-        WARN("Please check the user ID you requested is exist or not.");
+        WARN("用户信息修改失败");
+        WARN("请确保你输入了正确的ID");
     }
 
     public static void SUCCEEDED_ADMIN_MODIFY_USER(AuthSession authSession) {
-        INFO("Successfully modified this user's account information.");
+        INFO("成功以管理员权限修改指定用户信息");
         SHOW_USER(authSession);
     }
 
@@ -212,15 +212,15 @@ public class HintMaker {
         ConsoleLogger.RETURN_DEEPPURPLE(construction.getDescription());
         String[] usages = construction.getUsage().split(";");
         if (usages.length > 1){
-            RETURN_HELP_BLOCK("multiple sub command:" ,
-                    "as common: " + usages[0],
-                    "as admin: " + usages[1]);
+            RETURN_HELP_BLOCK("检测到多个命令变体:" ,
+                    "COMMON: " + usages[0],
+                    "ADMIN: " + usages[1]);
             return;
         }
         if (construction == Commands.CAR || construction == Commands.ADMIN ||
                 construction == Commands.PROFILE || construction == Commands.USER){
-            RETURN_HELP_BLOCK("multiple sub command:" , construction.getUsage(),
-                    "use HELP <sub command> for more info");
+            RETURN_HELP_BLOCK("检测到多个命令变体:" , construction.getUsage(),
+                    "使用 help <子命令> 来查看详细信息");
             return;
         }
         RETURN_HELP_BLOCK(construction.getUsage());
